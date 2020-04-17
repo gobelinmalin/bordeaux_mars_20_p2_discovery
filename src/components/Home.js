43ad9d9de3db26023f8d88559bac461d61cacdb0
Emeline <mariemeline.paus@gmail.com>
2020-04-17 17:18:04 +0200

    Add routes on react for contact and home

---
+++ src/components/Home.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+
+class Home extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Home</h1>
+        <Link to="/">Home</Link>
+        <Link to="/contact">Contact</Link>
+      </div>
+    );
+  }
+}
+
+export default Home;
