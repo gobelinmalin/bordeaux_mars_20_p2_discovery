82d48f71ddf781b344760ea24de902305a176a24
wilder <francois.yhuel@hotmail.fr>
2020-04-20 16:58:15 +0200

    add NavBar

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Route, Switch } from "react-router-dom";
 
+ 
 import Home from "./components/Home";
 import Contact from "./components/Contact";
 import Quiz from "./components/Quiz";
--- src/components/Home.js
+++ src/components/Home.js
@@ -6,11 +6,17 @@ class Home extends Component {
   render() {
     return (
       <div>
+        <nav className="NavBar">
         <Link to="/">Home</Link>
         <Link to="/contact">Contact</Link>
+        </nav>
         <div className="Home_slide_1">
           <h1>Discovery vous aide à trouver le meilleur endroit pour poser vos valises...</h1>
           <h3>Quiz ou hasard, le choix est vôtre !</h3>
+        </div>
+        <div className="Home_slide_2_and_3">
+          <div className="Home_slide_2"></div>
+          <div className="Home_slide_3"></div>
         </div>
       </div>
     );
