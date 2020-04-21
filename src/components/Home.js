3c859ebfe0d3ae671c25a736f808e6f7eddf30d9
wilder <francois.yhuel@hotmail.fr>
2020-04-21 15:29:25 +0200

    slide_2

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,31 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
+import axios from "axios";
+
 import "../style/css/Home.css";
 //const logoSvg from "../style/logo_discovery-01.svg"
 
 class Home extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      picture: null
+    }
+  }
+
+  getPicture(){
+    axios.get("https://pixabay.com/api/")
+    //extract DATA from the received response
+    .then(response => response.data)
+    // Use this data to update the state
+    .then(data =>{
+      console.log(data)
+      this.setState({
+        picture: data.results[0]
+      })
+    })
+  }
+
   render() {
     return (
       <div>
@@ -17,8 +39,14 @@ class Home extends Component {
           <h3>Quiz ou hasard, le choix est vôtre !</h3>
         </div>
         <div className="Home_slide_2_and_3">
-          <div className="Home_slide_2"></div>
-          <div className="Home_slide_3"></div>
+          <div className="Home_slide_2">
+            <h2> Révèle-toi à moi, et tu sauras ou tu vas !</h2>
+            <button> C'est parti pour le quiz !</button>
+          </div>
+          <div className="Home_slide_3">
+            <h2> Pas le temps de réfléchir? Laisse le destin agir !</h2>
+            <button className="button_globe"> Lance le globe !</button>
+          </div>
         </div>
       </div>
     );
