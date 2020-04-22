2bc7f738e905632c3329f8331a81a1cd0d88f070
Thomas BENARD <thomas0benard@gmail.com>
2020-04-22 16:08:08 +0200

    added divs to destination grece

--- src/components/Destination.js
+++ src/components/Destination.js
@@ -1,13 +1,49 @@
 import React, { Component } from "react";
+import axios from "axios";
+import { Link } from 'react-router-dom';
+
+import "../style/css/Destination.css";
 
 class Destination extends Component {
-  render() {
-    return (
-      <div>
-        <h1>Destination</h1>
-      </div>
-    );
-  }
+    constructor(props){
+        super(props);
+        this.state = {
+            picture: null
+        }
+        }
+
+        getPicture(){
+        axios.get("https://pixabay.com/api/")
+        //extract DATA from the received response
+        .then(response => response.data)
+        // Use this data to update the state
+        .then(data =>{
+            console.log(data)
+            this.setState({
+            picture: data.results[0]
+            })
+        })
+        }
+
+    render() {
+        return (
+        <div>
+            <div className="destination_head_picture">
+                <h1 className="destination_name">La Grèce</h1>
+                <h3>La Grèce</h3>
+            </div>
+            <div className="destination_description_1">
+                <h3>description 1</h3>
+            </div>
+            <div className="destination_description_2">
+                <h3>description 2</h3>
+            </div>
+            <div className="destination_partenaire">
+                <h3>partenaire</h3>
+            </div>
+        </div>
+        );
+    }
 }
 
 export default Destination;
