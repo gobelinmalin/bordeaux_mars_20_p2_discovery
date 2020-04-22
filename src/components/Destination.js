b6663841c45544e7f85e27294939aa37872fac46
Thomas BENARD <thomas0benard@gmail.com>
2020-04-22 15:39:38 +0200

    created Destination.js

---
+++ src/components/Destination.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class Destination extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Destination</h1>
+      </div>
+    );
+  }
+}
+
+export default Destination;
