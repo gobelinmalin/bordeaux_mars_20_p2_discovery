f4224cddb97c13e41c4f75f2410f4a2e7d8271af
enzo <enzo.seuve@laposte.net>
2020-04-23 11:29:18 +0200

    ajout texte

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -6,6 +6,7 @@ class Contact extends Component {
     return (
       <div>
         <h1>Contact</h1>
+        <p>orem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
       </div>
     );
   }
