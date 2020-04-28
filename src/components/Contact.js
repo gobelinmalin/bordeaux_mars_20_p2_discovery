e3620fc5e237fcd1990d518d8394bf77b641b2ed
Chris-css <christelle.savoca@gmail.com>
2020-04-28 23:58:31 +0200

    résolution de conflits

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -21,7 +21,7 @@ class Contact extends Component {
 
   render() {
     return (
-      <div>
+      <div className="background" >
         <h1>Contact</h1>
         <div className="container-texte-formulaire">
        <div className="contact-descriptif">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</div>
@@ -31,7 +31,7 @@ class Contact extends Component {
         <input type="email" name="field2" placeholder="Votre email" />
         <div className="message">Votre message</div>
         <textarea placeholder="" onkeyup="adjust_textarea(this)"></textarea>
-        <input value={this.state.value} onChange={this.handleChange} type="submit" value="Envoyer" />
+        <input onChange={this.handleChange} type="submit" value="Envoyer" />
   </form>
 </div>
       </div>
