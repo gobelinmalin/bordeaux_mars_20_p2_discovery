2d9cb6aa97c2c76947e77da68084001a89d5916a
Thomas BENARD <thomas0benard@gmail.com>
2020-05-04 09:57:06 +0200

    uncommented import

--- src/components/Destination.js
+++ src/components/Destination.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
-//import axios from "axios";
-//import { Link } from 'react-router-dom';
+import axios from "axios";
+import { Link } from 'react-router-dom';
 
 import "../style/css/Destination.css";
 
