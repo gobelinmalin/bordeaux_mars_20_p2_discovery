469732598e4639e42d43610966b180fc26251489
enzo <enzo.seuve@laposte.net>
2020-05-04 14:07:18 +0200

    modif

--- src/App.js
+++ src/App.js
@@ -1,22 +1,29 @@
 import React from "react";
 import { Route, Switch } from "react-router-dom";
 
+ 
 import Home from "./components/Home";
 import Contact from "./components/Contact";
 import Quiz from "./components/Quiz";
 import AboutUs from "./components/AboutUs";
+import NavBar from "./components/NavBar";
+import Footer from "./components/Footer";
+import Destination from "./components/Destination"
 
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
+      <NavBar />
       <Switch>
         <Route exact path="/" component={Home} />
         <Route path="/contact" component={Contact} />
-        <Route path="/quiz" component={Quiz} />
         <Route path="/AboutUs" component={AboutUs} />
+        <Route path="/quiz/:id" component={Quiz} />
+        <Route path="/destination/:id" component={Destination} />
       </Switch>
+      <Footer />
     </div>
   );
 }
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,8 +1,30 @@
 import React, { Component } from "react";
-import { Link } from "react-router-dom";
+import axios from "axios";
+import { Link } from 'react-router-dom';
+
 import "../style/css/Home.css";
 
 class Home extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      picture: null
+    }
+  }
+
+  getPicture(){
+    axios.get("https://pixabay.com/api/")
+    //extract DATA from the received response
+    .then(response => response.data)
+    // Use this data to update the state
+    .then(data =>{
+      console.log(data)
+      this.setState({
+        picture: data.results[0]
+      })
+    })
+  }
+
   render() {
     return (
       <div>
@@ -13,6 +35,22 @@ class Home extends Component {
         <div className="Home_slide_1">
           <h1>Discovery vous aide à trouver le meilleur endroit pour poser vos valises...</h1>
           <h3>Quiz ou hasard, le choix est vôtre !</h3>
+        </div>
+        <div className="Home_slide_2_and_3">
+          <div className="Home_slide_2">
+              <div>
+                <h2>Révèle-toi à moi,</h2>
+            <p>et tu sauras où tu vas !</p>
+              </div>
+            <Link to="/quiz/0" className="button_quiz">C'est parti pour le quiz !</Link>
+          </div>
+          <div className="Home_slide_3">
+            <div>
+              <h2>Pas le temps de réfléchir ?</h2>
+              <p>Laisse le destin agir !</p>
+            </div>
+            <button className="button_globe">Lance le globe !</button>
+          </div>
         </div>
       </div>
     );
