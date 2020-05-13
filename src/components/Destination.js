8dbef51adadce41b15195ddb1e491df7ede7e4c1
Thomas BENARD <thomas0benard@gmail.com>
2020-05-13 08:35:19 +0200

    blurred partenaire background

--- src/components/Destination.js
+++ src/components/Destination.js
@@ -36,12 +36,15 @@ class Destination extends Component {
                             <div className="image_destination" style={{backgroundImage: `url(${destination.image2})`}}></div>
                             <div className="image_destination" style={{backgroundImage: `url(${destination.image3})`}}></div>
                         </div>
-                        <div style={{backgroundImage: `url(${destination.image4})`}} className="destination_partenaire">
-                            <p className="partenaire_text">Retrouvez nos liens partenaires <br /> si vous souhaitez planifier votre voyage</p>
-                            <a href={destination.url} target="_blank" rel="noopener noreferrer" >
-                                <button className="partenaire_button">Envolez-vous!</button>
-                            </a>
-                            <Link to="/quiz/1" className="return_to_quiz">je retente ma chance</Link>
+                        <div className="destination_partenaire">
+                            <div className="partenaire_content">
+                                <p className="partenaire_text">Retrouvez nos liens partenaires <br /> si vous souhaitez planifier votre voyage</p>
+                                <a href={destination.url} target="_blank" rel="noopener noreferrer" >
+                                    <button className="partenaire_button">Envolez-vous!</button>
+                                </a>
+                                <Link to="/quiz/1" className="return_to_quiz">je retente ma chance</Link>
+                            </div>
+                            <div style={{backgroundImage: `url(${destination.image4})`}} className="background_partenaire"></div>
                         </div>
                     </div>
                 )}
