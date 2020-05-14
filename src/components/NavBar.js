faab7eb76d4d87868d0b85124671514328d144c9
Chris-css <christelle.savoca@gmail.com>
2020-05-14 09:40:41 +0200

    ajout menu burger version mobile

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,27 +1,43 @@
 import React from 'react';
 import { Link } from "react-router-dom";
+import { slide as Menu } from 'react-burger-menu';
 
 import logoSvg from "../style/logo_discovery-01.svg";
 
 import "../style/css/NavBar.css";
 
 
-const NavBar = () => {
-  return (
-    <nav className="NavBar">
-      <Link to="/" >
-        <img src={logoSvg} className="NavBar_logo"  alt="logo"/>
-      </Link>
-      
-      
-      <div className="NavBar_links">
-        <Link className="NavBar_link" to="/">Accueil</Link>
-        <Link className="NavBar_link" to="/AboutUs">A propos</Link>
-        <Link className="NavBar_link" to="/quiz/1">Quiz</Link>
-        <Link className="NavBar_link" to="/contact">Contact</Link>
-      </div>
-    </nav>
-  )
+class NavBar extends React.Component {
+  showSettings (event) {
+    event.preventDefault();
+  }
+
+  render () {
+    return (
+      <nav className="NavBar">
+        <Link to="/" >
+          <img src={logoSvg} className="NavBar_logo"  alt="logo"/>
+        </Link>
+        
+        
+        <div className="NavBar_links">
+          <Link className="NavBar_link" to="/">Accueil</Link>
+          <Link className="NavBar_link" to="/AboutUs">A propos</Link>
+          <Link className="NavBar_link" to="/quiz/1">Quiz</Link>
+          <Link className="NavBar_link" to="/contact">Contact</Link>
+        </div>
+  
+        <div className="Menu_burger" >
+            <Menu right>
+              <a id="home" className="menu-item" href="/">Home</a>
+              <a id="about" className="menu-item" href="/about">About</a>
+              <a id="contact" className="menu-item" href="/contact">Contact</a>
+              <a onClick={ this.showSettings } className="menu-item--small" href="/">Settings</a>
+            </Menu>
+        </div>
+      </nav>
+    )
+  }
 }
 
 export default NavBar;
